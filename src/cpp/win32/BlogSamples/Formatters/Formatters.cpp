db019324e73b003205af8de6e5e7109a4050a7c6
Vasilios Magriplis <crownmonarch@hotmail.com>
2022-08-06 22:44:11 +1000

    Messing with fixed strings

--- src/cpp/win32/BlogSamples/Formatters/Formatters.cpp
+++ src/cpp/win32/BlogSamples/Formatters/Formatters.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
+#include <vector>
 #include <format>
 #include <windows.h>
 
@@ -41,6 +42,20 @@ std::string ToString(const std::wstring& wstr)
     return strTo;
 }
 
+struct Converter
+{
+	Converter(const wchar_t* v) {}
+};
+
+template <>
+struct std::formatter<Converter> : std::formatter<std::string>
+{
+	auto format(Converter wstr, format_context& ctx)
+	{
+		return formatter<std::string>::format(std::format("{}", ""), ctx);
+	}
+};
+
 template <>
 struct std::formatter<std::wstring> : std::formatter<std::string>
 {
@@ -49,6 +64,16 @@ struct std::formatter<std::wstring> : std::formatter<std::string>
         return formatter<std::string>::format(std::format("{}", ToString(wstr)), ctx);
     }
 };
+
+
+
+namespace std
+{	
+	/*std::string format(std::_Fmt_string<std::string> left, const wchar_t* val)
+	{
+		return "A";
+	}*/
+}
 
 template <>
 struct std::formatter<const wchar_t*> : std::formatter<std::string>
@@ -59,12 +84,84 @@ struct std::formatter<const wchar_t*> : std::formatter<std::string>
 	}
 };
 
+template <>
+struct std::formatter<const wchar_t(*)[2]> : std::formatter<std::string>
+{
+	auto format(auto wstr, format_context& ctx)
+	{
+		return formatter<std::string>::format(std::format("{}", ""), ctx);
+	}
+};
+
+template <>
+struct std::formatter<const wchar_t(&)[2]> : std::formatter<std::string>
+{
+	auto format(const auto wstr, format_context& ctx)
+	{
+		return formatter<std::string>::format(std::format("{}", ""), ctx);
+	}
+};
+
+template<unsigned N>
+struct FixedString {
+	char buf[N + 1]{};
+	constexpr FixedString(char const* s) {
+		for (unsigned i = 0; i != N; ++i) buf[i] = s[i];
+	}
+	constexpr operator char const* () const { return buf; }
+};
+template<unsigned N> FixedString(char const (&)[N])->FixedString<N - 1>;
+
+template<FixedString T>
+class Foo {
+	static constexpr char const* Name = T;
+public:
+	void hello() const {};
+};
+
+
+
+template <size_t N>
+struct H
+{
+	constexpr H(const char (&arg)[N])
+	{
+
+	}
+
+	constexpr H(const wchar_t(&arg)[N])
+	{
+
+	}
+
+	void Blah() {}
+};
+template<size_t N> H(char const (&)[N])->H<N>;
+
+template<H T>
+class Foo2 {
+
+public:
+	void hello() const { };
+};
+
 int main(int argc, char* args[]) try
 {
+	Foo<"Hello!"> foo;
+	foo.hello();
+
+	Foo2<"P"> p;
+	//H a = "A";
+	//H b = L"A";
+	//H c = L"A";
+
     const std::wstring wstr = L"and I'm a wide string";
 	const wchar_t* blah = L"and I'm also a wide string";
-	std::cout << std::format("I am a narrow string {}.\n", wstr);
-	std::cout << std::format("I am a narrow string {}.\n", blah);
+	//std::cout << std::format("I am a narrow string {}.\n", wstr);
+	//std::cout << std::format("I am a narrow string {}.\n", blah);
+	//std::cout << std::format("I am a narrow string {}.\n", "A", 1);
+	//std::cout << std::format("I am a narrow string {} {}.\n", &L"A", (const wchar_t*)L"A");
+	//std::cout << std::format("I am a narrow string {} {}.\n", L"A", (const wchar_t*)L"A");
 
     return 0;
 }
